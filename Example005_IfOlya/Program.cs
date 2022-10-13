ef4020887ca1b2b187c603057e5227bdf6eaff24
Olga Serbul <Olsen.zika@mail.ru>
2022-10-13 12:03:26 +0300

    commit Example005_IfOlya project

---
+++ Example005_IfOlya/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "оля")
+{
+    Console.WriteLine("Ура, это же ОЛЯ!");
+}
+else
+{
+    Console.Write("Привет,  ");
+    Console.WriteLine(username);
+}
